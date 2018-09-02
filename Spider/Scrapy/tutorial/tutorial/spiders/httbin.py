2ab8e94820013c8de2ba8167033c74ab653b95b5
张炎 <970730254@qq.com>
2018-09-02 23:55:17 +0800

    更新

---
+++ Spider/Scrapy/tutorial/tutorial/spiders/httbin.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class HttbinSpider(scrapy.Spider):
+    name = 'httpbin'
+    allowed_domains = ['httpbin.org']
+    start_urls = ['http://httpbin.org/get']
+
+    def parse(self, response):
+        self.logger.debug(response.text)
+        self.logger.debug("Status Code : " + str(response.status))
